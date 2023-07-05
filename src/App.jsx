920eb789add96fa01247eb0ba513452896f573c2
SergeyRO <sergey_r.o@mail.ru>
2023-07-05 23:15:24 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css'
+import '../css/main.css'
+import { Component } from 'react'
+
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+class ShopItemClass extends Component
+{
+  constructor(props){
+    super(props);
+    this.item = props;
+  }
+   render(){
+    return (
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+        <div className="price">{item.currency}{parseFloat(item.price).toFixed(2)}</div>
+        <button>Добавить в корзину</button>
+      </div></div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />
+      </div>
+    </div>
+  )
+}
+
+export default App
